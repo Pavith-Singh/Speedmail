3f33da6df32733cbd4afc90d0e0fa914e5f52c1a
Pavith-Singh <pavith.singh@education.nsw.gov.au>
2025-04-28 20:24:37 +1000

    restart

--- Speedmail/backend/database.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('encrypted_database_speedmail.db');
-
-db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE, username_encrypted TEXT, password_hashed TEXT)');
-});
-
-module.exports = db;
